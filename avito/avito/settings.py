3be9907359bf3b6e2a382cb842bd3731d5479f2b
Pavel Ryaguzov <pashattaguzov@yandex.ru>
2023-12-07 19:33:50 +0500

    1

--- avito/avito/settings.py
+++ avito/avito/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'avito.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'avito_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- avito/avitoapp/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from avitoapp.models import Ad
-
-
-class AdSerializer(ModelSerializer):
-    class Meta:
-        model = Ad
-        fields = ['name', 'description', 'price', 'address', 'user', 'phone']
