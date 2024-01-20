678e447de5f7797f304a95b894937a80d13d0fa8
Pavel Ryaguzov <pashajobber@gmail.com>
2024-01-20 22:31:27 +0500

    ad rest

--- avito/avito/settings.py
+++ avito/avito/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'django_filters',
+
+    'myapiapp.apps.MyapiappConfig',
     'avitoapp.apps.AvitoappConfig',
     'myauth.apps.MyauthConfig',
 ]
@@ -144,5 +148,12 @@ CACHES = {
 }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ]
+}
 
 
